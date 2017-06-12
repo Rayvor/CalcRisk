a1a5de63332693316cf3cd19d230ebd241a9ee37
Куксенков Андрей <neo.qlive@gmail.com>
2017-06-12 14:58:17 +0300

    fix preResults

--- CalcRisk/preResultsForm.cs
+++ CalcRisk/preResultsForm.cs
@@ -28,18 +28,18 @@ namespace CalcRisk
             }
 
             dataGridMatrixCov.Rows.Add(calc.GetPUPCount + 1);
-
-            for (int i = 0; i < calc.GetPUPCount + 1; i++)
+            //матрица ковариаций
+            for (int i = 0; i <= calc.GetPUPCount; i++)
             {
                 dataGridMatrixCov.Rows[0].Cells[i].Value = i.ToString();
                 dataGridMatrixCov.Rows[i].Cells[0].Value = i.ToString();
             }
 
-            for (int i = 0; i < calc.GetPUPCount; i++)
-                for (int j = 0; j < calc.GetPUPCount; j++)
+            for (int i = 1; i <= calc.GetPUPCount; i++)
+                for (int j = 1; j <= calc.GetPUPCount; j++)
                 {
-                    dataGridMatrixCov.Rows[i + 1].Cells[j + 1].Value = Math.Round(calc.GetCov[j, i], 2);
-                    dataGridMatrixCov.Rows[j + 1].Cells[i + 1].Value = Math.Round(calc.GetCov[j, i], 2);
+                    dataGridMatrixCov.Rows[i].Cells[j].Value = Math.Round(calc.GetCov[j, i], 2);
+                    dataGridMatrixCov.Rows[j].Cells[i].Value = Math.Round(calc.GetCov[j, i], 2);
                 }
 
             for (int i = 0; i < calc.GetPUPCount + 1; i++)
@@ -48,10 +48,10 @@ namespace CalcRisk
                 col.Width = 70;
                 dataGridRandomValues.Columns.Add(col);
             }
-            dataGridRandomValues.Rows.Add(500);
-
-            for (int i = 0; i < calc.GetPUPCount; i++)
-                for (int j = 0; j < 500; j ++)
+            dataGridRandomValues.Rows.Add(501);
+            //сгенерированные велечины
+            for (int i = 1; i <= calc.GetPUPCount; i++)
+                for (int j = 1; j <= 500; j ++)
                 {
                     dataGridRandomValues.Rows[0].Cells[i].Value = i.ToString();
                     dataGridRandomValues.Rows[j].Cells[0].Value = j.ToString();
@@ -69,12 +69,12 @@ namespace CalcRisk
             }
             dataGridMoneyFlows.Rows.Add(2);
             dataGridClearFlows.Rows.Add(2);
-
-            for (int i = 0; i < calc.GetPUPCount - 1; i++)
+            //Денежные потоки
+            for (int i = 0; i <= calc.GetPUPCount - 1; i++)
             {
-                var cell = Math.Round(calc.GetInputStr[17, i], 3);
+                var cell = Math.Round(calc.GetInputStr[18, i + 1], 3);
                 dataGridMoneyFlows.Rows[1].Cells[i].Value = cell;
-                cell = Math.Round(calc.GetInputStr[16, i], 3);
+                cell = Math.Round(calc.GetInputStr[17, i + 1], 3);
                 dataGridClearFlows.Rows[1].Cells[i].Value = cell;
                 dataGridMoneyFlows.Rows[0].Cells[i].Value = (i + 1).ToString();
                 dataGridClearFlows.Rows[0].Cells[i].Value = (i + 1).ToString();
